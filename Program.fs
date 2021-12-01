e9d69d3081ed146b805181ed0b170a7df480a0e9
Anton Surkis <antonsurkis@gmail.com>
2021-12-01 22:55:35 +0300

    Implement stream approximation

--- Program.fs
+++ Program.fs
@@ -1,15 +1,24 @@
 ﻿open System
 
+// let inputPoints =
+//     Seq.initInfinite (fun _ -> Console.ReadLine())
+//     |> Seq.takeWhile (fun s -> s <> null)
+//     |> Seq.map (fun s -> s.Split [| ';' |])
+//     |> Seq.map (Array.map Double.TryParse)
+//     |> Seq.choose (function
+//         | [| (true, x); (true, y) |] -> Some(x, y)
+//         | _ ->
+//             printfn "Point should be written in \"<number>; <number>\" format"
+//             None)
+
 let inputPoints =
-    Seq.initInfinite (fun _ -> Console.ReadLine())
-    |> Seq.takeWhile (fun s -> s <> null)
-    |> Seq.map (fun s -> s.Split [| ';' |])
-    |> Seq.map (Array.map Double.TryParse)
-    |> Seq.choose (function
-        | [| (true, x); (true, y) |] -> Some(x, y)
-        | _ ->
-            printfn "Point should be written in \"<number>; <number>\" format"
-            None)
+    seq {
+        (-2.0, 4.0)
+        (-1.0, 1.0)
+        (0.0, 0.0)
+        (1.0, 1.0)
+        (2.0, 4.0)
+    }
 
 let linearApprox x1 y1 x2 y2 xs = y1 + (y2 - y1) * (xs - x1) / (x2 - x1)
 
@@ -19,20 +28,48 @@ let linear =
     |> Seq.tail
     |> Seq.map (fun ((x1, y1), (x2, y2)) -> x2, linearApprox x1 y1 x2 y2)
 
+let approximator = linear
+
 let start = -2.0
-let finish = 1.9
-let approxPoints = 11
+let finish = 2.0
+let approxPoints = 21
 
 let sampleXs =
-    seq { 0 .. approxPoints - 1 }
-    |> Seq.map (
+    [ 0 .. approxPoints - 1 ]
+    |> List.map (
         float
         >> linearApprox 0.0 start (float (approxPoints - 1)) finish
     )
 
-let approximator = linear
+(*
+    Input points are a sequence,
+    since the process should work as a pipe.
+    They should also be only evaluated once,
+    and Seq.cache is not free.
+    Therefore iterating through them
+    using a loop is preferrable.
+*)
+let samplesSplit =
+    seq {
+        let mutable samples = sampleXs
 
-approximator
-|> Seq.map (printfn "%A")
+        (*
+            Here we assign a function for interpolation
+            to the sample coordinated
+            based on the interval it falls into
+        *)
+        for xUp, f in approximator do
+            let left =
+                samples
+                |> List.takeWhile (fun x -> x < xUp)
+                |> List.map (fun x -> x, f)
+
+            yield! left
+            samples <- List.skip left.Length samples
+    }
+
+samplesSplit
+|> Seq.map (fun (x, f) -> x, f x)
+|> Seq.map (fun (x, y) -> printfn "%f %f" x y)
 |> Seq.length
 |> ignore
