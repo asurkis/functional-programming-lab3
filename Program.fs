cca069f4445d1cc7c3ec7ca4b2a1c75b97347665
Anton Surkis <antonsurkis@gmail.com>
2021-12-01 20:28:26 +0300

    Implement reading requence once

--- Program.fs
+++ Program.fs
@@ -14,17 +14,11 @@ let inputPoints =
         printfn "Accessing %A" x
         x)
 
-let splines =
-    Seq.map2 (fun (x1, y1) (x2, y2) -> (x2, (fun x -> (y2 - y1) * (x - x1) / (x2 - x1))))
-    <| inputPoints
-    <| Seq.tail inputPoints
-
-let rec calcSampleY splines x =
-    (splines
-     |> Seq.filter (fun (xr, _) -> x < xr)
-     |> Seq.head
-     |> snd)
-    <| x
+let linear =
+    inputPoints
+    |> Seq.scan (fun (_, a) b -> (a, b)) ((0.0, 0.0), (-infinity, 0.0))
+    |> Seq.tail
+    |> Seq.map (fun ((x1, y1), (x2, y2)) -> x2, fun x -> (y2 - y1) * (x - x1) / (x2 - x1))
 
 let sampleXs =
     seq { 0 .. approxPoints - 1 }
@@ -33,17 +27,25 @@ let sampleXs =
         + (finish - start) * float i
           / float (approxPoints - 1))
 
-let sampleYs =
-    sampleXs |> Seq.map (calcSampleY splines)
-
-Seq.zip sampleXs sampleYs
-|> Seq.map (sprintf "%A; ")
-|> Seq.fold (+) ""
+linear
+|> Seq.map (fun (x, f) ->
+    sampleXs
+    |> Seq.choose (fun sx -> if sx < x then Some(f sx) else None))
+|> Seq.concat
+|> Seq.map (fun x ->
+    printfn "%A" x
+    true)
+|> Seq.contains false
 |> ignore
-// |> printfn "[ %s]"
 
 open System
 
 let lines =
     Seq.initInfinite (fun _ -> Console.ReadLine())
-lines |> Seq.map (fun x -> printfn "%A" x; x) |> Seq.contains null |> ignore
+
+lines
+|> Seq.map (fun x ->
+    printfn "%A" x
+    x)
+|> Seq.contains null
+|> ignore
